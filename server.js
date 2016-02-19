faef077af837fe52877a09323ca5e691da5d9d17
Mark Korver <mwkorver@gmail.com>
2016-02-19 11:29:41 -0600

    add env vars in server.js

--- server.js
+++ server.js
@@ -8,94 +8,138 @@ var http = require('http'),
 	fs = require('fs'),
 	path = require('path'),
     request = require('request').defaults({ encoding: null }),
-    url = require('url'),  
-    findRemoveSync = require('find-remove');
+    url = require('url'),
+    os = require('os');
+  
+    //findRemoveSync = require('find-remove');
 
-    // get keys here, do not put this in ver control.
-	//AWS.config.loadFromPath('./config.json'); 
+// for local testing, if mac, change to darwin.
+if (os.platform() == 'win32'){
+	// load environment vars from local file
+	var env = require('./env.js');
+	// get keys here, do not put config.json in ver control. Add to gitignore
+	AWS.config.loadFromPath('./config.json');
+} else {
+	// load from EB's environment vars
+	// This is the S3 bucket that serves TMS requests. 
+	// It needs to be setup as a website and have a redirect rule
+	// pointed at this application
+	var bucketName = process.env.BUCKET_NAME; 
+	// Server name, typically an ELB in front of WMS servers 
+	// if necessary should include the map file path
+	// http://yournamehere-1977199279.us-east-1.elb.amazonaws.com/cgi-bin/mapserv?map=/data/map/mapfiles/mapfilename.map&
+	var wmsServer = process.env.WMS_SERVER;
+	// WMS request map layers
+	var mapLayers = process.env.MAP_LAYERS;
+}
 
-// this is the underlying ELB where WMS servers live
-var wmsServer = 'mapserver-docker-743146079.us-east-1.elb.amazonaws.com/cgi-bin/mapserv?map=/data/map/mapfiles/naip_docker.map&';
-// this bucket is setup as website, also some levels have 1 day lifecycle.
-var bucketname = "workshop-tms";  
+console.log('S3 Bucket: ' + process.env.BUCKET_NAME);
+console.log('WMS Server: ' + process.env.WMS_SERVER);
+console.log('Map Layer: ' + process.env.MAP_LAYERS);
 
- var mimeTypes = {
+var port = process.env.PORT || 8888
+
+var mimeTypes = {
     "html": "text/html",
-    "jpeg": "image/jpeg",
     "jpg": "image/jpeg",
+    "tif": "image/tif",
     "png": "image/png"};   
 
 http.createServer(function (req, res) {
 	console.log('-------------------------------------------');
+
+	// control for favicon request from browsers
+	if (req.url === '/favicon.ico') {
+		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+		res.end();
+		console.log('favicon requested');
+		return;
+	}
+	
 	var pathObject = url.parse(req.url,true).path;
 	console.log('pathObject: ' + pathObject);
 	var queryObject = url.parse(req.url,true).query;
     console.log('queryObject: ' + queryObject);
 
 	var test = false,
+	// a few geographically dispersed tile names for testing
 	testtilename = "17/20996/85306.jpg"; // utm10 seattle
 	//testtilename = "14/4958/6060.jpg"; // boston
 	//testtilename = "14/3887/10128.jpg"; // kansas city
 
 	// checks to see if tile object and debug param test exists
-	// if no tile name uses built-in tile name and put in debug mode
+	// if no tile name uses built-in tile name and sets to debug mode
 	if (typeof queryObject.tile !== 'undefined'){
 		tilename = queryObject.tile;
 		console.log('tilename in if block: ' + tilename);
 		if (typeof queryObject.test !== 'undefined'){
+			console.log('check for test: ' + queryObject.test);
 			test = true;				
 		}
 	} else {
-		console.log('Test mode');
 		test = true;
+		console.log('No tile query object, putting in test mode');
 		tilename = '1.0.0/tms-mercator-naip/' + testtilename;
 		console.log('no url value, using builtin test tilename: ' + tilename);				
 	} 
 
-	// add a prefix to tilename to write to diff part of targe bucket for testing
-	if(test){tilename = 'test-' + tilename};
-	console.log('tilename: ' + tilename);
-
+	try{
 	// parses the url
-	var level = tilename.split('/').slice(2)[0], 
-		col = tilename.split('/').slice(3)[0],
-		tmp = tilename.split('/').slice(4)[0],
-		row = parseInt(tmp.split('.').slice(0)[0]),
-		ext = tilename.split('.').pop(),
-		localfilepath = './tmp/' + level + '-' + col + '-' + row + '.' + ext;
+		var level = tilename.split('/').slice(2)[0], 
+			col = tilename.split('/').slice(3)[0],
+			tmp = tilename.split('/').slice(4)[0],
+			row = parseInt(tmp.split('.').slice(0)[0]),
+			ext = tilename.split('.').pop(),
+			// local file name for writing to disk
+			localfilepath = './tmp/' + level + '-' + col + '-' + row + '.' + ext;
+
+  	} catch (e) {
+	    res.writeHead(500, {'Content-Type': 'text/plain'});
+	    res.write('Server error\n');
+	    console.log('Error parsing url:' + e)
+	    res.end();
+		return;
+	}
+
 
 	console.log('localfilepath: ' + localfilepath);
 
 	// include Klokan TMS functions:
 	eval(fs.readFileSync('globalMercator.js')+'');
-
   	var mercator = MercatorUtils();
 
-	//this function builds WMS request from tile name.
+	//this function builds WMS request from a TMS tile name.
 	function getTileUrl(level, row, col) {
 		var mercBounds = mercator.tileBounds(row, col, level);
-		wmsreq = wmsServer + "&SERVICE=WMS&LAYERS=utm11-100pct&SRS=epsg:3857&BBOX=" 
+		wmsreq = process.env.WMS_SERVER + "&SERVICE=WMS&LAYERS=" + process.env.MAP_LAYERS
+		+  "&SRS=epsg:3857&BBOX=" 
 		+ mercBounds[1] + "," + mercBounds[0] + "," + mercBounds[3] + "," + mercBounds[2] 
-		+ "&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/jpeg&WIDTH=256&HEIGHT=256";
+		+ "&VERSION=1.1.1&REQUEST=GetMap&FORMAT=" 
+		+ mimeTypes[path.extname(localfilepath).split(".").reverse()[0]]
+		+ "&WIDTH=256&HEIGHT=256";
 		return (wmsreq);
   	}
-
-	// get reference to S3 client 
-	var s3 = new AWS.S3();
 
 	// Infer the image type.
 	var typeMatch = tilename.match(/\.([^.]*)$/);
 	if (!typeMatch) {
-		console.error('unable to infer image type for key ' + srcKey);
+		console.error('unable to infer image type for key ' + tilename);
+		res.writeHead(400, {'Content-Type': 'text/plain'} );
+		res.write('unable to infer image type from object key\n');
+		res.end();
 		return;
 	}
 	var imageType = typeMatch[1];
 	if (imageType != "jpg" && imageType != "png") {
-		console.log('skipping non-image ' + srcKey);
+		console.error('skipping non-image ' + tilename);
+		res.writeHead(400, {'Content-Type': 'text/plain'} );
+		res.write('Error, non-image request\n');
+		res.end();
 		return;
 	}
 
-	// need to do this async, get the data via wms req first, then write data
+
+	// get image via wms req first, then write data back and put to S3
 	async.waterfall([
 		function geturl(next){	
 			//builds wms request url
@@ -114,34 +158,35 @@ http.createServer(function (req, res) {
 			try {
 				//check to see if file exists
 		    	stats = fs.lstatSync(localfilepath); // throws if path doesn't exist
-				console.log('localfile exists');
 								
 				    if (test) {
 				    	var starttime = new Date().getTime();
-				 		res.writeHead(200);
+				 		res.writeHead(200, {'Content-Type': 'text/html'});
 						res.write('<html><body>');
-						res.write('tilename:<br>' + tilename + '<br>');
-						res.write('WMS request:<br>' + wmsrequest + '<br>');
-						res.write('Resulting tile:<br>');
+						res.write('Tilename:<br>' + tilename + '<br>');
+						res.write('S3 Bucket:<br>' + process.env.BUCKET_NAME + '<br>');
+						res.write('WMS Request:<br>' + wmsrequest + '<br>');
+						res.write('Resulting Image Tile:<br>');
 						res.write('<img src="' + wmsrequest + '">');
-						res.write('</body></html');						
+						res.write('</body></html>');						
 				  		res.end();
 					} else {
 						// upload image to S3		
-						var params = {Bucket: bucketname, 
+						var params = {Bucket: process.env.BUCKET_NAME, 
 							Key: tilename,
-							//ACL: 'private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control',
 							ACL: 'public-read',
 							ContentType: mimeTypes[path.extname(localfilepath).split(".").reverse()[0]], 
-							//RequestPayer: 'requester',
 							//Body: new Buffer('...') || 'STRING_VALUE' || streamObject,
 							Body: fs.createReadStream(localfilepath) 
 							//CacheControl: 'STRING_VALUE',
 						};
 
+						// get reference to S3 client 
+						var s3 = new AWS.S3();
+
 						s3.putObject(params, function(err, data) {
-							if (err) console.log(err, err.stack); 
-							else console.log('s3 put data: ' + data);
+							if (err) console.log('Error uploading to S3: ' + err);
+							else console.log('Uploaded tile to S3');
 						});
 
 						//write image data to HTTP response
@@ -151,9 +196,8 @@ http.createServer(function (req, res) {
 					    fs.createReadStream(localfilepath).pipe(res);
 					}
 			  	} catch (e) {
-				    res.writeHead(404, {'Content-Type': 'text/plain'});
-				    res.write('404 Not Found\n');
-				    res.write('error:' + e);
+				    res.writeHead(500, {'Content-Type': 'text/plain'});
+				    res.write('Server error\n');
 				    console.log('error:' + e)
 				    res.end();
 			    return;
@@ -163,23 +207,17 @@ http.createServer(function (req, res) {
 		], function(err) {
 			if (err) {
 				console.error(
-					'Unable to upload tile to: ' + bucketname + '/' + tilename +
-					' due to an error: ' + err
-				);
-			} else {
-				console.log(
-					'Successfully uploaded to ' + bucketname + '/' + tilename
-				);
-			}
+					'Unable to upload tile to: ' + bucketName + '/' + tilename );
+			} 
 		}
 	);
 
-	console.log('immediatlely before findRemove');
-	var result = findRemoveSync('./tmp', {age: {seconds: 30}, extensions: '.jpg'});
-	console.log('findRemove result: ' + result);
+	//console.log('immediatlely before findRemove');
+	//var result = findRemoveSync('./tmp', {age: {seconds: 30}, extensions: '.jpg'});
+	//console.log('findRemove result: ' + result);
 
-//}).listen(8080);
-}).listen(process.env.PORT || 8888);
+}).listen(port);
+console.log('running on port: ' + port);
 
 
 
